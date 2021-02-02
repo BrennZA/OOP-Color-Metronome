97026b6b0e8587013cedbcd18a31802f87163703
Brenn_ZA <bswanepoel4025@gmail.com>
2021-02-02 20:21:26 +0200

    set display inner text to input values with updateDisplay function

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     const tempo = form.elements.tempo.value;
     const note = form.elements.note.value;
+    updateDisplay(tempo, note);
 });
+
+
+const updateDisplay = function(tempo, note) {
+    const tempoDisplay = document.querySelector("#metronomeDisplay__tempo > span");
+    tempoDisplay.innerText = tempo;
+    const noteDisplay = document.querySelector("#metronomeDisplay__note > span");
+    noteDisplay.innerText = note;
+}
